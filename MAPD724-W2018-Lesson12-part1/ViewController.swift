d242448911f4e8b215c341c48be443d57a0f3e46
Tejal1386 <tejalmca1386@gmail.com>
2018-04-16 20:29:28 -0400

    Added code to make the see in mapApp button work - closes #6

--- MAPD724-W2018-Lesson12-part1/ViewController.swift
+++ MAPD724-W2018-Lesson12-part1/ViewController.swift
@@ -30,6 +30,19 @@ class ViewController: UIViewController {
         self.map.addAnnotation(ann)
     }
 
+    @IBAction func seeinMapApp(_ sender: UIButton) {
+        let placemark = MKPlacemark(coordinate: self.mapLocation, addressDictionary: nil)
+        let mapitem = MKMapItem(placemark: placemark)
+        mapitem.name = "A really icy place"
+        
+        //opens our location in map app
+        mapitem.openInMaps(launchOptions: [
+            MKLaunchOptionsMapTypeKey: MKMapType.standard.rawValue,
+            MKLaunchOptionsMapCenterKey: self.map.region.center,
+            MKLaunchOptionsMapSpanKey: self.map.region.span
+            ])
+    }
+    
 
 }
 
