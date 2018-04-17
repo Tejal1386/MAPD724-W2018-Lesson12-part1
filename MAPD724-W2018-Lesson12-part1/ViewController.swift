83a8e2621f2eafb15a142d768f165711e87c9588
Tejal1386 <tejalmca1386@gmail.com>
2018-04-16 20:07:25 -0400

    added code to make toronto default map location - closes #5

--- MAPD724-W2018-Lesson12-part1/ViewController.swift
+++ MAPD724-W2018-Lesson12-part1/ViewController.swift
@@ -4,10 +4,24 @@ import UIKit
 import  MapKit
 
 class ViewController: UIViewController {
+   
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    //To Fix
+    let Latitude = 43.6532
+    let Longitude = -79.3832
+    var delta = 5.0
+    
+    let mapLocation = CLLocationCoordinate2DMake(43.6532, -79.3832)
 
     override func viewDidLoad() {
         super.viewDidLoad()
  
+        let location  = CLLocationCoordinate2DMake(Latitude, Longitude)
+        let span = MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
+        let reg = MKCoordinateRegionMake(location, span)
+        self.map.region = reg
     }
 
 
