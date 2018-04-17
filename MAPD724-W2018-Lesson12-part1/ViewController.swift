4f6fa71ff85a83bcb47664d943b6bf2bdb725c89
Tejal1386 <tejalmca1386@gmail.com>
2018-04-16 21:21:59 -0400

    code added to get latitude, longitude and delta value from user - closes #7, closes #8

--- MAPD724-W2018-Lesson12-part1/ViewController.swift
+++ MAPD724-W2018-Lesson12-part1/ViewController.swift
@@ -8,6 +8,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var map: MKMapView!
     
+    @IBOutlet weak var textLatitude: UITextField!
+    @IBOutlet weak var textLongitude: UITextField!
+    
+    @IBOutlet weak var textMagnification: UITextField!
+    
     //To Fix
     let Latitude = 43.6532
     let Longitude = -79.3832
@@ -18,7 +23,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
  
-        let location  = CLLocationCoordinate2DMake(Latitude, Longitude)
+     /*   let location  = CLLocationCoordinate2DMake(Latitude, Longitude)
         let span = MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
         let reg = MKCoordinateRegionMake(location, span)
         self.map.region = reg
@@ -27,7 +32,7 @@ class ViewController: UIViewController {
         ann.coordinate = self.mapLocation
         ann.title = "Toronto"
         ann.subtitle = "A Place that's really cool"
-        self.map.addAnnotation(ann)
+        self.map.addAnnotation(ann) */
     }
 
     @IBAction func seeinMapApp(_ sender: UIButton) {
@@ -42,6 +47,24 @@ class ViewController: UIViewController {
             MKLaunchOptionsMapSpanKey: self.map.region.span
             ])
     }
+    
+    
+    @IBAction func searchLocation(_ sender: UIButton) {
+        
+        let location  = CLLocationCoordinate2DMake((textLatitude.text as! NSString).doubleValue, (textLongitude.text as! NSString).doubleValue)
+        
+        let span = MKCoordinateSpan(latitudeDelta: (textMagnification.text as! NSString).doubleValue, longitudeDelta: (textMagnification.text as! NSString).doubleValue)
+        let reg = MKCoordinateRegionMake(location, span)
+        self.map.region = reg
+        
+        let ann = MKPointAnnotation()
+        ann.coordinate = self.mapLocation
+        ann.title = "Toronto"
+        ann.subtitle = "A Place that's really cool"
+        self.map.addAnnotation(ann)
+        
+    }
+    
     
 
 }
